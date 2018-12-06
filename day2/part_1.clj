e739745881f12b735ee53f6bbf319ba1fe328d7e
Neil Lambert <neil.lambert@fundingcircle.com>
2018-12-06 11:39:36 -0800

    day2-part1

---
+++ day2/part_1.clj
@@ -0,0 +1,27 @@
+(def box-ids (clojure.string/split-lines (slurp "input.txt")))
+
+(def letter-frequencies
+  "list of maps containing {character number-of-occurrences}
+   for each Box ID."
+  (map frequencies box-ids))
+
+(defn has-n-duplicate-items?
+  "Returns true if a frequency map has at least 1 set of n duplicates"
+  [n]
+  (fn [letter-freq-map]
+    (let [occurrences (-> (vals letter-freq-map) frequencies)]
+      (>= (get occurrences n 0) 1))))
+
+(def contains-a-pair
+  (filter
+    (has-n-duplicate-items? 2)
+    letter-frequencies))
+
+(def contains-a-triple
+  (filter
+    (has-n-duplicate-items? 3)
+    letter-frequencies))
+
+(def answer
+  (* (count contains-a-pair) (count contains-a-triple)))
+
