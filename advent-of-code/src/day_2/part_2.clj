6ec756cbb176df0dadb99d2b49a81bd1f02fe0f7
Neil Lambert <neil.lambert@fundingcircle.com>
2018-12-06 16:51:59 -0800

    day2, part2

--- advent-of-code/src/day_2/part_2.clj
+++ advent-of-code/src/day_2/part_2.clj
@@ -1,27 +1,20 @@
 (ns day-2.part-2
-  (:require [clojure.set :refer [intersection union difference]]))
+  (:require [clojure.set :refer [intersection union difference]]
+            [clojure.math.combinatorics :refer [combinations]]))
 
 (def box-ids (clojure.string/split-lines (slurp "src/day_2/input.txt")))
 
-(def letter-frequencies (map frequencies box-ids))
+(defn off-by-one?
+  [[s1 s2]]
 
-;; combine all unique letter frequencies between the 2 strings
-;; find all the matching frequencies between the two strings
-;; subtract the matches from the total uniques.
-;; if you only have 2 uniques, each string is only off by one letter
-;; if you have 0, the strings are matching
-;; if you have more than 2, each string differs by more than 1 letter.
-(defn off-by-one-letter
-  [freq1 freq2]
-  (let [all-letters (union
-                      (into #{} freq1)
-                      (into #{} freq2))
-        shared-letters (intersection
-                         (into #{} freq1)
-                         (into #{} freq2))]
-    (when (= 2
-           (count
-             (difference
-               all-letters
-               shared-letters)))
-      (map first shared-letters))))
+  (let [letter-comparisons (filter
+                             char?
+                             (map #(when (= %1 %2) %1) s1 s2))]
+
+    (when (= 1 (- (count s1)
+                  (count letter-comparisons)))
+      (reduce str letter-comparisons))))
+
+(def answer (keep
+              off-by-one?
+              (combinations box-ids 2)))
